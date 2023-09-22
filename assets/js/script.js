a0a8002efc09812120071e306275d3ba11e9df39
Matheus Gabriel Dalla Valle <126087969+DallaVal@users.noreply.github.com>
2023-09-22 16:55:05 -0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
+var largura = window.innerWidth
+function carregar(){
+    if(largura <= 980 ){
+        var mostrarmais = document.getElementById('row2')
+        mostrarmais.style = 'display: none; margin: 4px;'
+        var destaque = document.getElementById('destaque')
+        destaque.style = 'margin-top: 520px;'
+        var sobre = document.getElementById('sobre')
+        sobre.style = 'margin-top: 550px;'
+    }
+}
 
 
 function clicar(){
@@ -10,6 +21,33 @@ function clicar(){
     destaque.style = 'margin-top: 187px'
     textmostrarmenos.style = 'display: none'
     tirarmostrarmenos.style = 'display: flex'
+    var sectionsobre = document.getElementById('sobre')
+    sectionsobre.style = 'margin-top: 260px'
+    if(largura <= 1600 && largura > 980){
+        var sobre = document.getElementById('sobre')
+        sobre.style = 'margin-top: -250px;'
+    } else if(largura <= 980){
+        var destaque = document.getElementById('destaque')
+        destaque.style = 'margin-top: 1300px;'
+        var sobre = document.getElementById('sobre')
+        sobre.style = 'margin-top: 1300px'
+        var row2 =  document.getElementById('row2')
+        row2.style = 'margin-top: 20px'
+        var pizza2 = document.querySelector('.pizza2')
+        pizza2.style = 'margin-top: 60px;'
+        var pizza3 = document.querySelector('.pizza3')
+        pizza3.style = 'margin-top: 60px;'
+        var pizza4 = document.querySelector('.pizza4')
+        pizza4.style = 'margin-top: 60px;'
+        var pizza5 = document.querySelector('.pizza5')
+        pizza5.style = 'margin-top: 60px;'
+        var pizza6 = document.querySelector('.pizza6')
+        pizza6.style = 'margin-top: 60px;'
+        var pizza7 = document.querySelector('.pizza7')
+        pizza7.style = 'margin-top: 60px;'
+        var pizza8 = document.querySelector('.pizza8')
+        pizza8.style = 'margin-top: 60px;'
+    }
 }
 
 
@@ -23,4 +61,15 @@ function segundoclick(){
     destaque.style = 'margin-top: 0px'
     textmostrarmenos.style = 'display: flex'
     tirarmostrarmenos.style = 'display: none'
+    var sectionsobre = document.getElementById('sobre')
+    sectionsobre.style = 'margin-top: 0px'
+    if(largura <= 1600 && largura > 980){
+        var sobre = document.getElementById('sobre')
+        sobre.style = 'margin-top: -480px;'
+    } else if(largura <= 980){
+        var destaque = document.getElementById('destaque')
+        destaque.style = 'margin-top: 520px;'
+        var sobre = document.getElementById('sobre')
+        sobre.style = 'margin-top: 550px;'
+    }
 }
